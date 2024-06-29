204678abe63c9f82a755ef19ca352c3dcd99cb59
Surya Agus Nanro <164362836+suryaagus9@users.noreply.github.com>
2024-06-29 10:45:09 +0800

    Delete project-final directory

--- project-final/backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '12345678'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///tutoring.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'super-secret' 
--- project-final/backend/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-user_bp = Blueprint('user', __name__)
-student_bp = Blueprint('student', __name__)
-tutor_bp = Blueprint('tutor', __name__)
-class_bp = Blueprint('class', __name__)
-join_bp = Blueprint('join', __name__)
-
-from . import join_routes, user_routes, tutor_routes, student_routes, class_routes
